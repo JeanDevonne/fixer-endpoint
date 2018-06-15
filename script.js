cbec94bf63572d6349e0daa3fcd0c9a10bc98bc1
Jean <20094194@sistemasunica.edu.pe>
2018-06-15 00:22:49 -0500

    consumiendo endpoint

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+const request = superagent;
+const API_ENDPOINT = "http://data.fixer.io/api/latest?access_key=0b72d3e9686d0de7753c3dbd99d5168c&format=1";
+function fetchPrice() {
+    request
+      .get(API_ENDPOINT)
+      .then(function(response) {
+          const responseObject = response.body;
+          const EurToPen = responseObject.rates.PEN;
+          const priceBox = document.querySelector("#price");
+          priceBox.textContent = `1 Euro = S/. ${EurToPen.toFixed(2)} Nuevos Soles`;
+      })
+      .catch(function() {
+          alert("Ooops! algo ha salido mal");
+      })
+}
+button.addEventListener("click", fetchPrice);
